869e321f27c46725d06b99e1bb092c383d519480
Robert Benavidez <murabitbenavidez@gmail.com>
2023-08-24 13:26:55 -0400

    handlers

--- main.go
+++ main.go
@@ -3,28 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
-}
 
 const portNumber = ":8080"
 
+// main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
